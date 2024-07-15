f9bf94849f5c6dd9d1337f24809c172ac70bba75
arfanovi <arfanovi.gub1@gmail.com>
2024-07-15 17:11:33 +0600

    Get Request With URL Query

--- 04-Request-Get-Method/index.js
+++ 04-Request-Get-Method/index.js
@@ -1,17 +1,25 @@
-
 let express = require('express');
 let app = express();
 
 
 
 
-app.get('/', function (req, res) {
-    res.end('This is simple get request')
-})
 
+app.get('/', function (req, res) {
+
+
+    // Extract query parameters from the URL
+    let firstName = req.query.firstName;
+    let lastName = req.query.lastName;
+
+    // Respond with the concatenated firstName and lastName
+    res.end(firstName + " " + lastName);
+
+
+});
 
 
 
 app.listen(1500, function() {
-    console.log(('Server Run Success'))
-})
+    console.log('Server Run Success');
+});
