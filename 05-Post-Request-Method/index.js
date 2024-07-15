3047a822d5c68a9ff2e4e0944dfa5af12aa97785
arfanovi <arfanovi.gub1@gmail.com>
2024-07-15 17:53:07 +0600

    Post Request With Header Properties

--- 05-Post-Request-Method/index.js
+++ 05-Post-Request-Method/index.js
@@ -6,7 +6,7 @@ let app = express();
 
 
 
-
+/*
 app.post("/", function(req, res) {
     // res.send("This is Simple post request");
 
@@ -17,8 +17,22 @@ app.post("/", function(req, res) {
     res.send(firsName + " " + lastName);
 })
 
+*/
 
 
+//  Post Request With Header Properties
+
+
+app.post("/", function(req, res) {
+
+    let userName = req.header("userName");
+    let password = req.header("password");
+
+    res.send("User Name:" + userName + "Password:" + password);
+
+    
+
+})
 
 
 
